f5aa924db6a9bbd3a16b1bb0f45b91985ca46f45
hexaalif <hexaalif2020@gmail.com>
2023-04-12 05:37:50 +0600

    printing slash and backslash

--- printAssignment.c
+++ printAssignment.c
@@ -8,5 +8,6 @@ int main()
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     printf("Recently I heard that you’ve achieved 95%% marks in your exam.\nThis is brilliant!\nI wish you’ll shine in your life!\tGood luck with all the barriers(/\\) in your life.");
+    printf("\\/\\");
     return 0;
 }
