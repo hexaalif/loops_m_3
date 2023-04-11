7c24c8d10cf7b09ad9066d48d75bb65a78836b04
hexaalif <hexaalif2020@gmail.com>
2023-04-12 05:36:08 +0600

    first commit

---
+++ practice.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char k, h;
+    scanf("%c %c", &k, &h);
+    printf("%c\n %c\n", k, h);
+    return 0;
+}
---
+++ printAssignment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    printf("Recently I heard that you’ve achieved 95%% marks in your exam.\nThis is brilliant!\nI wish you’ll shine in your life!\tGood luck with all the barriers(/\\) in your life.");
+    return 0;
+}
